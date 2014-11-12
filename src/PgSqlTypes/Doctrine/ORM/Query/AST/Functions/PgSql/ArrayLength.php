99df12b4b0c2c9d9998b6903cdff3bfc2a0c7d42
Dmitry Khayrutdinov <haid84@gmail.com>
2014-11-12 11:09:56 +0300

    Some functions for PgSql arrays

---
+++ src/PgSqlTypes/Doctrine/ORM/Query/AST/Functions/PgSql/ArrayLength.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace PgSqlTypes\Doctrine\ORM\Query\AST\Functions\PgSql;
+
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+
+class ArrayLength extends FunctionNode
+{
+    public $expr = null;
+    public $dimension = null;
+
+    /**
+     * @override
+     */
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return sprintf(
+            'ARRAY_LENGTH(%s, %s)',
+            $this->expr->dispatch($sqlWalker),
+            $sqlWalker->walkLiteral($this->dimension)
+        );
+    }
+
+    /**
+     * @override
+     */
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->expr = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_COMMA);
+        $this->dimension = $parser->Literal();
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
---
+++ src/PgSqlTypes/Doctrine/ORM/Query/AST/Functions/PgSql/Contained.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace PgSqlTypes\Doctrine\ORM\Query\AST\Functions\PgSql;
+
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\SqlWalker;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+
+class Contained extends FunctionNode
+{
+    public $left = null;
+    public $right = null;
+
+    /**
+     * @override
+     */
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return sprintf(
+            '(%s <@ %s)',
+            $this->left->dispatch($sqlWalker),
+            $this->right->dispatch($sqlWalker)
+        );
+    }
+
+    /**
+     * @override
+     */
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->left = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_COMMA);
+        $this->right = $parser->ArithmeticPrimary();
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
